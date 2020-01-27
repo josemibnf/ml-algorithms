34b91362c6a8ef03526cbd3d5ecd4957a7838bdd
jal04 <josemi.bnf@outlook.es>
2020-01-27 19:10:21 +0100

    hcluster

--- k_means.py
+++ k_means.py
@@ -53,6 +53,38 @@ class bicluster:
 		self.id = id
 		self.distance = distance
 
+def hcluster(rows, distance=pearson):
+	distances={} # stores the distances for efficiency
+	currentclustid=-1 # all except the original items have a negative id
+
+	# Clusters are initially just the rows
+	clust = [bicluster(rows[i], id=i) for i in range(len(rows))]
+	
+    while len(clust)>1: #stop when there is only one cluster left
+        lowestpair = (0, 1)
+		closest = distance(clust[0].vec,clust[1].vec)
+	# loop through every pair looking for the smallest distance
+		for i in range(len(clust)):
+			for j in range(i+1,len(clust)):
+			# distances is the cache of distance calculations
+				if (clust[i].id,clust[j].id) not in distances:
+					distances[(clust[i].id,clust[j].id)] = distance(clust[i].vec,clust[j].vec)
+
+                                d = distances[(clust[i].id,clust[j].id)]
+				if d < closest:
+					closest = d
+					lowestpair = (i,j)
+		# calculate the average of the two clusters
+		mergevec=[(clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0 for i in range(len(clust[0].vec))]	
+		#create the new cluster
+		newcluster=bicluster(mergevec,left=clust[lowestpair[0]],right=clust[lowestpair[1]],distance=closest,id=currentclustid)
+		# cluster ids that weren’t in the original set are negative
+		currentclustid-=1
+		del clust[lowestpair[1]]
+		del clust[lowestpair[0]]
+		clust.append(newcluster)
+        return clust[0]
+
 def printclust(clust, labels=None, n=0):
         # indent to make a hierarchy layout
         for i in range(n):
